115085ac13d50f92618a74190da61d24de92c417
Jjieeoo <jie.chen@estudiantat.upc.edu>
2022-04-27 18:28:47 +0200

    movimiento

--- Pruebas/move.py
+++ Pruebas/move.py
@@ -1,23 +1,22 @@
 import pygame
 
 pygame.init()
-window = pygame.display.set_mode((1920, 1080))
+window = pygame.display.set_mode((1200, 700))
 clock = pygame.time.Clock()
 
 class Prueba:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self):
+
         self.path = [(1187, 146), (400, 141), (394, 325), (882, 341), (878, 537), (160, 517), (132, 155)]
         self.path_ini= self.path[0]
-        self.speed= 1
+        self.speed= 5
 
     def move(self, pos):
         pore = self.path[0]
-        dir = pygame.math.Vector2(pore) - pos
+        dir = pygame.math.Vector2(self.path[0]) - pos
         if dir.length() <= self.speed:
-            pos = self.path_ini
+            pos = self.path[0]
             self.path.append(self.path[0])
             self.path.pop(0)
         else:
@@ -25,16 +24,22 @@ class Prueba:
             new_pos = pygame.math.Vector2(pos) + dir
             pos = (new_pos.x, new_pos.y)
         return pos
+    def ataque(self):
+        if pos == self.path[-1]:
+
+        else:
+            pass
 
 if __name__ == '__main__':
     run = True
-    P = Prueba(1, 1)
+    P = Prueba()
+    pos=(1187, 146)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        pos = P.move((1187, 146))
+        pos = P.move(pos)
 
         window.fill(0)
         pygame.draw.circle(window, "red", pos, 20)
